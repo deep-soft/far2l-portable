ee7b35093037cd948f41c0d391492ffbca88f807
spvkgn <spvkgn@users.noreply.github.com>
2024-05-11 08:49:36 +0500

    update scripts

--- build_far2l.sh
+++ build_far2l.sh
@@ -8,17 +8,18 @@ BUILD_DIR=build
 if [[ $(awk -F= '/^ID=/ {print $2}' /etc/os-release) == "alpine" ]]; then
   CMAKE_OPTS+=( "-DMUSL=ON" )
 fi
-if [[ "$STANDALONE" == "true" ]]; then
+if [[ "$WXGUI" == "false" ]]; then
   CMAKE_OPTS+=( "-DUSEWX=no" )
 fi
 if [[ "$PLUGINS_EXTRA" == "true" ]]; then
-  for plug in netcfgplugin sqlplugin processes ; do
-    git clone --depth 1 https://github.com/VPROFi/$plug.git && \
-    ( cd $plug && \
-      find . -mindepth 1 -name 'src' -prune -o -exec rm -rf {} + && \
-      mv src/* . && rm -rf src )
-    echo "add_subdirectory($plug)" >> CMakeLists.txt
-  done
+  ( cd $REPO_DIR/far2l && \
+    for plug in netcfgplugin sqlplugin processes ; do
+      git clone --depth 1 https://github.com/VPROFi/$plug.git && \
+      ( cd $plug && \
+        find . -mindepth 1 -name 'src' -prune -o -exec rm -rf {} + && \
+        mv src/* . && rm -rf src )
+      echo "add_subdirectory($plug)" >> CMakeLists.txt
+    done )
 fi
 if [[ "$PLUGINS" == "false" ]]; then
   CMAKE_OPTS+=( "-DCOLORER=no -DNETROCKS=no -DALIGN=no -DAUTOWRAP=no -DCALC=no \
@@ -26,25 +27,25 @@ if [[ "$PLUGINS" == "false" ]]; then
     -DINCSRCH=no -DINSIDE=no -DMULTIARC=no -DSIMPLEINDENT=no -DTMPPANEL=no" )
 fi
 
-QUILT_PATCHES=$REPO_DIR/patches quilt push -a
+( cd $REPO_DIR/far2l && QUILT_PATCHES=$REPO_DIR/patches quilt push -a )
 
 mkdir -p $BUILD_DIR && \
-( cd $BUILD_DIR && \
-  cmake -G Ninja \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_INSTALL_PREFIX=/usr \
-    -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache \
-    -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache \
-    ${CMAKE_OPTS[@]} .. && \
-    ninja && ninja install/strip ) && \
-
-tar cJvf $REPO_DIR/far2l.tar.xz -C $REPO_DIR/AppDir .
+cmake -G Ninja -S $REPO_DIR/far2l -B$REPO_DIR/far2l/$BUILD_DIR \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DCMAKE_VERBOSE_MAKEFILE=ON \
+  -DCMAKE_C_COMPILER_LAUNCHER=/usr/bin/ccache \
+  -DCMAKE_CXX_COMPILER_LAUNCHER=/usr/bin/ccache \
+  ${CMAKE_OPTS[@]} && \
+  ninja -C $REPO_DIR/far2l/$BUILD_DIR install/strip && \
 
 if [[ "$STANDALONE" == "true" ]]; then
-  ( cd $BUILD_DIR/install && ./far2l --help >/dev/null && bash -x $REPO_DIR/make_standalone.sh ) && \
-  ( cd $REPO_DIR && makeself --keep-umask far2l/$BUILD_DIR/install $PKG_NAME.run "FAR2L File Manager" ./far2l && \
-    tar cvf ${PKG_NAME/_${VERSION}}.run.tar $PKG_NAME.run )
-fi
+  mkdir -p $REPO_DIR/standalone && cp -a $REPO_DIR/far2l/$BUILD_DIR/install/* $REPO_DIR/standalone && \
+  ( cd $REPO_DIR/standalone && ./far2l --help >/dev/null && bash -x $REPO_DIR/make_standalone.sh ) && \
+  makeself --keep-umask --nomd5 --nocrc $REPO_DIR/standalone $PKG_NAME.run "FAR2L File Manager" ./far2l && \
+  tar cvf ${PKG_NAME/_${VERSION}}.run.tar $PKG_NAME.run
+fi && \
+
+tar cJvf $REPO_DIR/far2l.tar.xz -C $REPO_DIR/AppDir .
 
 ccache --max-size=50M --show-stats
